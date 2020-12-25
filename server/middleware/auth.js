56813b7932fc4e2644a1ec9197fa7eed44463798
Vince <53957319+vincengai@users.noreply.github.com>
2020-12-25 10:42:37 -0800

    delete route setup

---
+++ server/middleware/auth.js
@@ -0,0 +1,8 @@
+//this middleware will run w/ our routes that will 
+//run before the function being passed into the route
+
+import jwt from 'jsonwebtoken';
+
+const auth = (req, res, next) => {
+    // console.log req
+}
